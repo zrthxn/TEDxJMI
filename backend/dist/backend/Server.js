6655f8d10b47defce2dfd0069b382ce2ba4dcd1d
Alisamar Husain <zrthxn@gmail.com>
2019-07-09 01:23:56 +0530

    Typescript config

---
+++ backend/dist/backend/Server.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const text_1 = require("./libs/text");
+const app = express_1.default();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send(text_1.text + 'holoahoaa');
+});
+app.listen(port, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on ${port}`);
+});
+//# sourceMappingURL=Server.js.map
