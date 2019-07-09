08892a194dae94b52ed0a96d313b2f3c0b9fa12e
Alisamar Husain <zrthxn@gmail.com>
2019-07-09 22:25:08 +0530

    Admin

--- backend/Server.ts
+++ backend/Server.ts
@@ -1,16 +1,16 @@
-import express from 'express'
-import { text } from './libs/text'
+const express = require('express')
+const { text } = require('./util/text')
 
 const app = express()
-const port = 3000;
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send(text+'holoahoaa');
-});
+  res.send(text+'holoahoaa')
+})
 
 app.listen(port, (err) => {
   if (err) {
-    return console.error(err);
+    return console.error(err)
   }
   return console.log(`Server is listening on ${port}`)
 })
