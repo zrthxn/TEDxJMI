02e142228039405f9b7bf5452578343d1d41cfcd
Alisamar Husain <zrthxn@gmail.com>
2019-07-30 19:58:23 +0530

    hwbf

--- apps/tedxjmi/src/App.test.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Ted from './Ted';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Ted />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
---
+++ apps/website/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
+import './App.css'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App
