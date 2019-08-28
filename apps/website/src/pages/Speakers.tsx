2d7013ab3ad3ff21a44a87f07ab6e285b7e190de
Azim Javed <thatazimjaved@gmail.com>
2019-08-28 15:52:51 +0530

    Speakers Name text box to be addded.

--- apps/website/src/pages/Speakers.tsx
+++ apps/website/src/pages/Speakers.tsx
@@ -9,19 +9,117 @@ export class Speakers extends Component {
     return (
       <article>
         <h2>Speakers</h2>
-        
+        <br/>
         <section>
           <CardContainer>
             <Card size="small">
-              <div style={{ padding: '1em' }}>
-                Heelo
+              <div className="tba">
+                To be announced
+                <br/><br/><br/><br/>
+                <div className="tedxjmi">
+                <span className="alpha"><b>TEDx</b></span>
+                <span className="beta">JMI</span>
+                </div>
               </div>
-            </Card>
-
+              </Card>
+              <Card size="small">
+              <div className="tba">
+                To be announced
+                <br/><br/><br/><br/>
+                <div className="tedxjmi">
+                <span className="alpha"><b>TEDx</b></span>
+                <span className="beta">JMI</span>
+                </div>
+              </div>
+              </Card>
+              <Card size="small">
+              <div className="tba">
+                To be announced
+                <br/><br/><br/><br/>
+                <div className="tedxjmi">
+                <span className="alpha"><b>TEDx</b></span>
+                <span className="beta">JMI</span>
+                </div>
+              </div>
+              </Card>
+              <Card size="small">
+              <div className="tba">
+                To be announced
+                <br/><br/><br/><br/>
+                <div className="tedxjmi">
+                <span className="alpha"><b>TEDx</b></span>
+                <span className="beta">JMI</span>
+                </div>
+              </div>
+              </Card>
+              <Card size="small">
+              <div className="tba">
+                To be announced
+                <br/><br/><br/><br/>
+                <div className="tedxjmi">
+                <span className="alpha"><b>TEDx</b></span>
+                <span className="beta">JMI</span>
+                </div>
+              </div>
+              </Card>
+              <Card size="small">
+              <div className="tba">
+                To be announced
+                <br/><br/><br/><br/>
+                <div className="tedxjmi">
+                <span className="alpha"><b>TEDx</b></span>
+                <span className="beta">JMI</span>
+                </div>
+              </div>
+              </Card>
+              <Card size="small">
+              <div className="tba">
+                To be announced
+                <br/><br/><br/><br/>
+                <div className="tedxjmi">
+                <span className="alpha"><b>TEDx</b></span>
+                <span className="beta">JMI</span>
+                </div>
+              </div>
+              </Card>
+              <Card size="small">
+              <div className="tba">
+                To be announced
+                <br/><br/><br/><br/>
+                <div className="tedxjmi">
+                <span className="alpha"><b>TEDx</b></span>
+                <span className="beta">JMI</span>
+                </div>
+              </div>
+              </Card>
+          </CardContainer>
+        </section>
+        <h3>2018</h3>
+        <section>
+          <CardContainer>
             <Card size="small">
-              <div style={{ padding: '1em' }}>
-                Heelo
-              </div>
+              <img alt="speaker" src="/res/speakers/KavitaBahl.jpg"/>
+            </Card>
+            <Card size="small">
+              <img alt="speaker" src="/res/speakers/NandanSaxena.jpg"/>
+            </Card>
+            <Card size="small">
+              <img alt="speaker" src="/res/speakers/RajeshSGokhale.jpg"/>
+            </Card>
+            <Card size="small">
+              <img alt="speaker" src="/res/speakers/RamakrishnaRamaSwamy.jpg"/>
+            </Card>
+            <Card size="small">
+              <img alt="speaker" src="/res/speakers/SadafHussain.jpg"/>
+            </Card>
+            <Card size="small">
+              <img alt="speaker" src="/res/speakers/SaeedNaqvi.jpg"/>
+            </Card>
+            <Card size="small">
+              <img alt="speaker" src="/res/speakers/TanmayaJain.jpg"/>
+            </Card>
+            <Card size="small">
+              <img alt="speaker" src="/res/speakers/VivekSharma.jpg"/>
             </Card>
           </CardContainer>
         </section>
