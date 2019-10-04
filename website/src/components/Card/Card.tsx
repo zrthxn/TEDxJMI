727840eb0efdf24b1b32373d7648d225d3d9618e
Azim Javed <thatazimjaved@gmail.com>
2019-10-04 15:01:21 +0530

    Modal/Card popups

--- website/src/components/Card/Card.tsx
+++ website/src/components/Card/Card.tsx
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 import './Card.css'
 
 interface CardProps {
-  size?: 'small' | 'medium' | 'large'
+  size?: 'small' | 'medium' | 'large',
+  linkTo?: string
 }
 
 export class Card extends Component<CardProps> {
   state = {
     style: {
-      
-    }
+
+    },
+    drawerOpened : false
   }
 
   styles = {
@@ -27,30 +29,51 @@ export class Card extends Component<CardProps> {
   }
 
   componentDidMount() {
-    if(this.props.size!==null)
+    if (this.props.size !== null)
       this.createStyle(this.props.size)
   }
-  
-  createStyle = (size?:CardProps['size']) => {
-    if(size!==undefined) var _size = this.styles.sizes[size]
 
-    this.setState(()=>{
+  createStyle = (size?: CardProps['size']) => {
+    if (size !== undefined) var _size = this.styles.sizes[size]
+
+    this.setState(() => {
       const currentStyle = this.state.style
       return {
         style: { ...currentStyle, ..._size }
       }
     })
   }
+  clickHandler(link:string){
+    if(link=='') return
+    let drawerRef = document.getElementById(link);
+    if(drawerRef==null) return;
+    if(this.state.drawerOpened){
+      drawerRef.className = drawerRef.className.split(' ')[0];
+      drawerRef.className += ' disappear';
+      this.setState({
+        drawerOpened : false
+      })
+    } else {
+      drawerRef.className = drawerRef.className.split(' ')[0];
+      drawerRef.className += ' appear';
+      this.setState({
+        drawerOpened: true
+      })
+    }
+  }
 
   render() {
     return (
-      <div className="card" style={ this.state.style }>
-        <div className="card-content">
-          {
-            this.props.children
-          }
+      <a onClick={(event)=>{let param = this.props.linkTo? this.props.linkTo: ' ';this.clickHandler(param)}}>
+        <div className="card" style={this.state.style}>
+          <div className="card-content">
+            {
+              this.props.children
+            }
+          </div>
+          <section className="drawer-shadow"></section>
         </div>
-      </div>
+      </a>
     )
   }
 }
--- website/src/pages/Speakers.tsx
+++ website/src/pages/Speakers.tsx
@@ -8,7 +8,7 @@ export class Speakers extends Component {
     return (
       <article>
         <h1>Speakers</h1>
-        <br/>
+        <br />
         <section className="center">
           <h2>To be Announced</h2>
         </section>
@@ -16,43 +16,49 @@ export class Speakers extends Component {
         <h3>2017</h3>
         <section>
           <CardContainer>
-            <Card size="medium">
-              <img alt="speaker" src="/assets/img/speakers/KavitaBahl.jpg"/>
+            <Card size="medium" linkTo="kbahl">
+              <img alt="speaker" src="/assets/img/speakers/KavitaBahl.jpg" />
               <div className="sub">Kavita Bahl</div>
+              <div className="drawer" id="kbahl">
+                <div className="close"><span></span></div>
+                  <h1>Kavita Bahl</h1>
+                  <img alt="speaker" src="/assets/img/speakers/KavitaBahl.jpg" />
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacus massa, ultricies quis enim vitae, varius dictum mi. Vivamus ac augue ornare ipsum bibendum dignissim vel eget mauris. Sed sed congue neque. Donec sit amet enim felis. Phasellus ultricies, turpis non volutpat viverra, dui tortor viverra tortor, ut vehicula nisl elit vitae dolor. Maecenas sit amet iaculis lectus. Cras non semper tellus. Duis iaculis laoreet erat. Nam semper nulla quis imperdiet placerat. Quisque suscipit purus non neque tincidunt, nec tristique lectus varius. Aliquam in lacus eleifend, hendrerit justo nec, pulvinar ipsum. Integer tortor urna, hendrerit vel eros a, lobortis venenatis lorem. Nulla facilisi. Duis a mattis massa.</p>
+              </div>
             </Card>
 
             <Card size="medium">
-              <img alt="speaker" src="/assets/img/speakers/NandanSaxena.jpg"/>
+              <img alt="speaker" src="/assets/img/speakers/NandanSaxena.jpg" />
               <div className="sub">Nandan Saxena</div>
             </Card>
 
             <Card size="medium">
-              <img alt="speaker" src="/assets/img/speakers/RajeshSGokhale.jpg"/>
+              <img alt="speaker" src="/assets/img/speakers/RajeshSGokhale.jpg" />
               <div className="sub">Rajesh S Gokhale</div>
             </Card>
 
             <Card size="medium">
-              <img alt="speaker" src="/assets/img/speakers/RamakrishnaRamaSwamy.jpg"/>
+              <img alt="speaker" src="/assets/img/speakers/RamakrishnaRamaSwamy.jpg" />
               <div className="sub">Ramakrishna Ramaswamy</div>
             </Card>
 
             <Card size="medium">
-              <img alt="speaker" src="/assets/img/speakers/SadafHussain.jpg"/>
+              <img alt="speaker" src="/assets/img/speakers/SadafHussain.jpg" />
               <div className="sub">Sadaf Hussain</div>
             </Card>
 
             <Card size="medium">
-              <img alt="speaker" src="/assets/img/speakers/SaeedNaqvi.jpg"/>
+              <img alt="speaker" src="/assets/img/speakers/SaeedNaqvi.jpg" />
               <div className="sub">Saeed Naqvi</div>
             </Card>
 
             <Card size="medium">
-              <img alt="speaker" src="/assets/img/speakers/TanmayaJain.jpg"/>
+              <img alt="speaker" src="/assets/img/speakers/TanmayaJain.jpg" />
               <div className="sub">Tanmaya Jain</div>
             </Card>
 
             <Card size="medium">
-              <img alt="speaker" src="/assets/img/speakers/VivekSharma.jpg"/>
+              <img alt="speaker" src="/assets/img/speakers/VivekSharma.jpg" />
               <div className="sub">Vivek Sharma</div>
             </Card>
           </CardContainer>
