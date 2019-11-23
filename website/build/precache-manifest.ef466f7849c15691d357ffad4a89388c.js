33ce585ad8a46dab1f15d6aca16a6009fa8cd465
Alisamar Husain <zrthxn@gmail.com>
2019-11-24 00:06:43 +0530

    build

---
+++ website/build/precache-manifest.ef466f7849c15691d357ffad4a89388c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbca42a1bf5df156923a984703e7ee1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16ec089c01e328eca208",
+    "url": "/static/css/main.ebeafff7.chunk.css"
+  },
+  {
+    "revision": "10dd1c0df0a6521dbf79",
+    "url": "/static/js/2.8468387e.chunk.js"
+  },
+  {
+    "revision": "16ec089c01e328eca208",
+    "url": "/static/js/main.8abd06b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
